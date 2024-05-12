c8921c2b298ce837f61b87cf0a727743895e3c9f
Asisiov <asisiov@gmail.com>
2024-05-13 01:12:35 +0300

    Delete MNN.framework directory

--- MNN.framework/Headers/Interpreter.hpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e2f726ad98f3d3c278bcf3f57a2c1cfd9ef813ed50e482e1cb62f76a33234531
-size 8431
--- MNN.framework/Headers/expr/Expr.hpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:02045779983fc655d3295cae5967c58bbbb6dd2abc768dc7659357856cf65fe5
-size 6718
