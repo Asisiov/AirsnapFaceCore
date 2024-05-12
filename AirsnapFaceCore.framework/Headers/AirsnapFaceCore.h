6f735932fb5a29c79797e1827d5e20b40f3fe655
Asisiov <asisiov@gmail.com>
2024-05-13 01:17:47 +0300

    Delete AirsnapFaceCore.framework directory

--- AirsnapFaceCore.framework/Headers/AirsnapFaceCore.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:44802279b9ee3221063f14bf2cfcaef4ccf0560b38712715b8390f070343f520
-size 568
--- AirsnapFaceCore.framework/Headers/FaceBox.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aea27f87924f5a0da6ac141d4563091f67fba75aedb808bfb8ca18ff2efa260b
-size 1630
