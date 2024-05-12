b734a430ebef61aa92f596bdf39f400c10acdab6
Asisiov <asisiov@gmail.com>
2024-05-13 01:13:53 +0300

    Delete opencv2.xcframework directory

--- opencv2.xcframework/ios-arm64/opencv2.framework/Headers/cvconfig.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:84927445ea2cace2a09b42334cc0061550fe57824c0dcc1cfe3e062c3106c355
-size 3201
--- opencv2.xcframework/ios-arm64/opencv2.framework/Headers/xfeatures2d/nonfree.hpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a07a1a68a616af290adcb95d1b0adb20dcbcc037b7e1b13e1b26e1fcb66ef224
-size 5391
