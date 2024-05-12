569ed48c891aad2d583c0ffe69f453bbf687e273
Asisiov <asisiov@gmail.com>
2024-05-12 22:26:48 +0300

    Add files via upload

---
+++ AirsnapFaceUI.framework/Headers/T5_PfcmUIFramework.h
@@ -0,0 +1,18 @@
+//
+//  T5_PfcmUIFramework.h
+//  T5_PfcmUIFramework
+//
+//  Created by IOS on 30/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for T5_PfcmUIFramework.
+FOUNDATION_EXPORT double T5_PfcmUIFrameworkVersionNumber;
+
+//! Project version string for T5_PfcmUIFramework.
+FOUNDATION_EXPORT const unsigned char T5_PfcmUIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <T5_PfcmUIFramework/PublicHeader.h>
+
+
